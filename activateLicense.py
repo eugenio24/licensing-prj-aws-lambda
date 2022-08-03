b75d58dd7f7733db139afad0f4372244378ef4ea
eugenio24 <eugenioferrari81@gmail.com>
2022-08-03 13:57:55 +0200

    Dev

---
+++ activateLicense.py
@@ -0,0 +1,55 @@
+import json
+import boto3
+import pymongo
+import sys
+import random
+import string
+import os
+
+def lambda_handler(event, context):
+    n_to_remove = len('/request')
+    response_topic = event['topic'][:-n_to_remove] + '/response'
+    mqttclient = boto3.client('iot-data', region_name='us-east-1')
+    
+    client = pymongo.MongoClient(os.environ['DB_URL']) 
+    try:    
+        client.admin.command('ping')
+    except ConnectionFailure:
+        response = mqttclient.publish(
+            topic = response_topic,
+            qos=1,
+            payload=json.dumps({"success": False, "message": "Server Error"})
+        )
+        return response
+
+    db = client.licensingdb
+    col = db.licensing
+    
+    hardware_id = event.get('hardware_id')
+    app_type = event.get('app_type')
+    
+    if hardware_id is None or app_type is None:
+        response = mqttclient.publish(
+            topic = response_topic,
+            qos=1,
+            payload= json.dumps({"success": False, "message": "Invalid request"})
+        )
+        return response
+    
+    print(f'Recived activate license request with args: hardware_id: {hardware_id} app_type: {app_type}')
+    
+    license_key = ''.join(random.choice(string.ascii_lowercase) for i in range(15))
+    
+    print(f'Generated license_key: {license_key}')
+    
+    col.insert_one({'hardware_id': hardware_id, 'app_type': app_type, 'license_key': license_key })
+
+    client.close()
+
+    response = mqttclient.publish(
+        topic = response_topic,
+        qos=1,
+        payload=json.dumps({"success": True, "validLicense": True, "license_key": license_key})
+    )
+    
+    return response
---
+++ checkLicense.py
@@ -0,0 +1,68 @@
+import json
+import boto3
+import pymongo
+import sys
+import random
+import string
+import os
+
+def lambda_handler(event, context):
+    n_to_remove = len('/request')
+    response_topic = event['topic'][:-n_to_remove] + '/response'
+    mqttclient = boto3.client('iot-data', region_name='us-east-1')
+    
+    client = pymongo.MongoClient(os.environ['DB_URL']) 
+    try:    
+        client.admin.command('ping')
+    except ConnectionFailure:
+        print("Cannot connect to MongoDB")
+        response = mqttclient.publish(
+            topic = response_topic,
+            qos=1,
+            payload=json.dumps({"success": False, "message": "Server Error"})
+        )
+        return response
+
+    db = client.licensingdb
+    col = db.licensing
+    
+    license_key = event.get('license_key')
+    hardware_id = event.get('hardware_id')
+    
+    if license_key is None or hardware_id is None:
+        response = mqttclient.publish(
+            topic = response_topic,
+            qos=1,
+            payload=json.dumps({"success": False, "message": "Invalid request"})
+        )
+        return response
+    
+    print(f'Recived check license request with args: license_key: {license_key} hardware_id: {hardware_id}')
+    
+    lic = col.find_one({'license_key': license_key})
+    
+    if lic is None:
+        response = mqttclient.publish(
+            topic = response_topic,
+            qos=1,
+            payload= json.dumps({"success": True, "validLicense": False, "message": "License not found"})
+        )
+        return response
+    
+    if lic["hardware_id"] != hardware_id:
+        response = mqttclient.publish(
+            topic = response_topic,
+            qos=1,
+            payload=json.dumps({"success": True, "validLicense": False, "message": "Hardware id not match"})
+        )
+        return response
+        
+    client.close()
+
+    response = mqttclient.publish(
+        topic = response_topic,
+        qos=1,
+        payload=json.dumps({"success": True, "validLicense": True })
+    )
+    
+    return response
